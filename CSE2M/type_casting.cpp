d448291ff59b4324864486aa37ef24c406d2c909
MananTheCoder <manangoyaljee@gmail.com>
2026-05-02 13:52:23 +0530

    Codes for CSE Batch till 2.5.26

--- CSE2M/type_casting.cpp
+++ CSE2M/type_casting.cpp
@@ -1,15 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+void c_style_typecasting()
 {
+    // int e = (int)e;
+    // cout << (f1 + a) << "\n";
+    // cout << a << "\n";
     int a = 2;
     float f1 = 2.6, f2 = 1.7;
     int b = (int)f1;
     int c = int(f1);
     cout << b << "\t" << c << "\n";
     int d = int(b);
-    // int e = (int)e;
-    // cout << (f1 + a) << "\n";
-    // cout << a << "\n";
+}
+
+void char_int_typecasting()
+{
+    char c1 = 'B', c2 = '1'; // 66 * 49 = 3234
+    int a = 97 + 256 * 49;
+    cout << char(a) << "\n";
+}
+
+int main()
+{
+    char_int_typecasting();
 }
