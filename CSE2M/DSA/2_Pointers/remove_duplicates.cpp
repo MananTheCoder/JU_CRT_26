75b81a17424147a0696fceadaf62ad6444f6495c
MananTheCoder <manangoyaljee@gmail.com>
2026-05-12 12:06:53 +0530

    Codes till CSE batch 12.5

--- CSE2M/DSA/2_Pointers/remove_duplicates.cpp
+++ CSE2M/DSA/2_Pointers/remove_duplicates.cpp
@@ -26,16 +26,47 @@ void print_vi(vi &v)
     cout << "\n";
 }
 
-int remove_duplicates(vi &v)
+int remove_duplicates(vi &v) // O(n+k)
 {
-    //
+    int d = 0, i = 1;
+    while (i < v.size()) // n
+    {
+        if (v[d] == v[i])
+        {
+            i++;
+        }
+        else
+        {
+            d++; // k
+            v[d] = v[i];
+        }
+    }
+    return (d + 1);
+}
+
+int remove_duplicates_sanitized(vi &v)
+{
+    int d = 0;
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (v[d] != v[i])
+        {
+            v[++d] = v[i];
+        }
+    }
+    return (d + 1);
 }
 
 void solve()
 {
     vi v = input_vi();
     int k = remove_duplicates(v);
-    print_vi(v);
+    forn(i, k)
+    {
+        cout << v[i] << "\t";
+    }
+    cout << "\n";
+    // print_vi(v);
 }
 
 int main()
